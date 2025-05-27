461e8b8b14530b172e56d5c2899bc13fd71b98a5
clarisse <clariamalotti@gmail.com>
2025-05-27 07:51:36 -0300

    Update ClienteDAO.java

--- AAIPOO/AAIPOO/src/model/ClienteDAO.java
+++ AAIPOO/AAIPOO/src/model/ClienteDAO.java
@@ -1,14 +1,14 @@
 package model;
- 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
- 
+
 import javax.swing.JOptionPane;
- 
+
 public class ClienteDAO {
 	public void salvar(Cliente cliente) {
-		String sql = "INSERT INTO Cliente(Nome, CPF, Email, Telefone, Endereço, codCliente) VALUES (?, ?, ?, ?, ?, ?)";
+		String sql = "INSERT INTO Cliente(Nome, CPF, Email, Telefone, Endereço) VALUES (?, ?, ?, ?, ?)";
 		Conexao.conectar();
 		try(Connection conn = Conexao.conexao; PreparedStatement stmt = conn.prepareStatement(sql)){
 			stmt.setString(1, cliente.getNome());
@@ -16,7 +16,6 @@ public class ClienteDAO {
 			stmt.setString(3, cliente.getEmail());
 			stmt.setString(4, cliente.getTelefone());
 			stmt.setString(5, cliente.getEndereco());
-			stmt.setInt(6,2);
 			JOptionPane.showMessageDialog(null, stmt.toString());
 			stmt.executeUpdate();
 			Conexao.desconectar();
@@ -25,3 +24,4 @@ public class ClienteDAO {
 		}
 	}
 }
+
